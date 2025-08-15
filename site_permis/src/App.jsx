267c153d7bc60452188fc1c5687f8223d951cf2b
Ameriesh <amerieninmazou@gmail.com>
2025-08-15 05:35:35 +0100

    Resolution des conflicts

--- site_permis/src/App.jsx
+++ site_permis/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     return (
-        <BrowserRouter>
+        <>
             <AppRoutes />
-        </BrowserRouter>
+        </>
+        
     );
 }
 
--- site_permis/src/routes/AppRoutes.jsx
+++ site_permis/src/routes/AppRoutes.jsx
@@ -1,16 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/home/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Accueil from '../pages/accueil/Accueil'
 import Services from '../pages/services/Services'
 import Contact from '../pages/contact/Contact'
 
 export default function AppRoutes () {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/accueil" element={<Accueil />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/accueil" element={<Accueil />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
