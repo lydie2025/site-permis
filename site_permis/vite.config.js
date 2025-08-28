2e0e79e9c7b243eb069892f410b82d5221e234c6
Ameriesh <amerieninmazou@gmail.com>
2025-08-28 08:47:15 +0100

    nouveau dossier

--- site_permis/vite.config.js
+++ site_permis/vite.config.js
@@ -6,16 +6,8 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
-  },
+  base: '/',
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
