96c33531f0d12ef109bac44906a51deb273823e1
Ameriesh <amerieninmazou@gmail.com>
2025-08-28 08:25:23 +0100

    mes modifs

--- site_permis/vite.config.js
+++ site_permis/vite.config.js
@@ -6,6 +6,16 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
